0e5aea763ec54fca8b9c1cc810e1f4bccc829b45
tiago-rods <t.alves.rodrigues@hotmail.com>
2026-05-29 23:09:25 -0300

    snacks

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -13,8 +13,14 @@ return {
             popup_border_style = "rounded",
             enable_git_status = true,
             enable_diagnostics = true,
+            ilesystem = {
+            filtered_items = {
+              hide_gitignore = false,
+              never_show = { ".git" },
+            },
+        }
         })
-		vim.keymap.set("n", "<C-n>", ":Neotree filesystem reveal right<CR>", {})
+		vim.keymap.set("n", "<C-n>", ":Neotree filesystem toggle right<CR>", {})
         vim.keymap.set('n', '<leader>n', ':Neotree filesystem reveal right<CR>', {noremap = true})
 		vim.keymap.set("n", "<leader>bf", ":Neotree buffers reveal float<CR>", {})
 	end,
